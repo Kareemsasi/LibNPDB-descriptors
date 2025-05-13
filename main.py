d6112247db60c0bb57ee95c8ed1130719dddc066
Kareemsasi <kareem.sasi@gmail.com>
2025-05-13 23:43:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -73,5 +73,7 @@ def index():
             results = calculate_descriptors(smiles)
     return render_template("index.html", results=results)
 
-if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=8080)
+if __name__ == '__main__':
+    import os
+    port = int(os.environ.get("PORT", 10000))  # Render sets PORT environment variable
+    app.run(host='0.0.0.0', port=port)
